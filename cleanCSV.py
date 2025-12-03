4f0a8345a130556678bed6ee4e768d5ab60f717a
mq20050522 <58794091+mq20050522@users.noreply.github.com>
2025-12-03 17:39:50 -0500

    Create cleanCSV.py

---
+++ cleanCSV.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import regexp_replace, col
+
+spark = SparkSession.builder     .appName("TwitterCSV")     .getOrCreate()
+
+path = "/storage/home/mfl5839/work/englishtweets.csv"  # or .csv, name doesn't matter
+
+df = spark.read     .option("header", "true")     .option("sep", ";")     .option("quote", '"')     .option("escape", '"')     .option("multiLine", "true")     .option("mode", "PERMISSIVE")     .csv(path)
+
+clean_df = df.withColumn(
+    "text",
+    regexp_replace(
+        regexp_replace(
+            regexp_replace(col("text"), r'[\r\n]', ' '),  # replace newlines with space
+            r'[\\\/]', ' '                                # replace backslashes and forward slashes with space
+        ),
+        r'"', '""'                                      # escape double quotes
+    )
+)
+
+(clean_df.write
+   .mode("overwrite")
+   .option("header", True)
+   .option("quoteAll", True)      # wrap everything in quotes
+   .option("escape", '"')         # escape quotes inside text
+   .option("multiLine", True)     # handle long text with internal newlines
+   .csv("/storage/home/mfl5839/work/output_folder"))
+
+clean_df = clean_df.select('origen','date','username','user_fullname','n_replies','n_likes','n_retweets','text','tweet_language')
+
+clean_df.write     .mode("overwrite")     .option("header", "true")     .csv("output_folder")
+
+
+# In[ ]:
+
+
+
+
